080e62cfa14e2edaad1cb4228c9fa6617bfdcb8f
Riyanoke <109645710+Riyanoke@users.noreply.github.com>
2022-11-09 13:38:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <h1>Aplikasi Pendataan</h1>
+    <h2>PC Lab</h2>
+    <h3> <a href=tambah.php>Tambah</a></h3>
+    <h3> <a href=komputer.php>Tampil</a></h3>
+    <h3> <a href=edit.php>Edit</a></h3>
+</center>
+</body>
+</html>
